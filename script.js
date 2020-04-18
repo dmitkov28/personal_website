0c9c25e95e27ea147baabb0139b3e8b02ffdd17d
Dimitar Mitkov <mitkov.dimitar@yahoo.com>
2020-04-18 20:17:25 +0300

    fixed hamburger

--- script.js
+++ script.js
@@ -6,35 +6,19 @@ const description = document.querySelector('.description');
 
 navButton.addEventListener('click', function(){
 
-    
 
     if(navContent.classList.contains('active')){
         navContent.classList.remove('active');
-
-document.querySelector('.bar.bar-2').style.opacity='1';
-document.querySelector('.bar.bar-2').style.transition='500ms';
-
-document.querySelector('.bar.bar-1').style.transform='rotate(0deg)';
-document.querySelector('.bar.bar-1').style.transition='500ms';
-
-document.querySelector('.bar.bar-3').style.transform='rotate(0deg)';
-document.querySelector('.bar.bar-3').style.transition='500ms';
-
-
-
+        document.querySelector('.bar-1').classList.remove('open-1');
+        document.querySelector('.bar-2').classList.remove('open-2');
+        document.querySelector('.bar-3').classList.remove('open-3');
 
     } else {
         navContent.classList.add('active');
-        document.querySelector('.bar.bar-2').style.opacity='0';
-document.querySelector('.bar.bar-2').style.transition='500ms';
-
-document.querySelector('.bar.bar-1').style.transform='rotate(45deg)translate(0.5em, 0em)';
-document.querySelector('.bar.bar-1').style.transition='600ms';
-document.querySelector('.bar.bar-1').style.transformOrigin='center';
-
-document.querySelector('.bar.bar-3').style.transform='rotate(-45deg)translate(0.7em, -0.35em)';
-document.querySelector('.bar.bar-3').style.transition='600ms';
-document.querySelector('.bar.bar-3').style.transformOrigin='center center';
+    document.querySelector('.bar-1').classList.add('open-1');
+    document.querySelector('.bar-2').classList.add('open-2');
+    document.querySelector('.bar-3').classList.add('open-3');
+     
     }
 
 });
